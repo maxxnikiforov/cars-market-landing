e099cca32f0d6c73703c52d1e55ebe1b03b65289
Max Nikiforov <maxxnikiforov@gmail.com>
2022-07-11 18:30:07 +0700

    almost finished

--- src/components/Email/Email.jsx
+++ src/components/Email/Email.jsx
@@ -1,34 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Container, Form, Button } from 'react-bootstrap';
 import './Email.scss';
 
-export const Email = () => (
-  <div className="email">
-    <Container className="email__container" id="email">
-      <div className="email__text">
-        <h3 className="email__text-title">
-          Haven&apos;t found a suitable vehicle?
-        </h3>
-        <p className="email__text-paragraph">
-          Sign up for our newsletter and be the
-          first to know when we publish new vehicle offers.
-        </p>
-      </div>
-      <Form
-        className="email__form"
-        onSubmit={event => event.preventDefault()}
-      >
-        <Form.Group className="mb-3" variant="dark" controlId="formBasicEmail">
-          <Form.Control
-            type="email"
-            placeholder="Enter your email"
-            className="email__input"
-          />
-        </Form.Group>
-        <Button variant="light" type="submit" className="email__button">
-          Subscribe now
-        </Button>
-      </Form>
-    </Container>
-  </div>
-);
+export const Email = () => {
+  const [input, setInput] = useState('');
+  const [email, setEmail] = useState('');
+
+  return (
+    <div className="email">
+      <Container className="email__container" id="email">
+        <div className="email__text">
+          <h3 className="email__text-title">
+            Haven&apos;t found a suitable vehicle?
+          </h3>
+          <p className="email__text-paragraph">
+            Sign up for our newsletter and be the
+            first to know when we publish new vehicle offers.
+          </p>
+        </div>
+        <Form
+          className="email__form"
+          onSubmit={(event) => {
+            event.preventDefault();
+            setInput('');
+            setEmail('Thanks for your contact');
+          }}
+        >
+          <Form.Group
+            className="mb-3"
+            variant="dark"
+            controlId="formBasicEmail"
+          >
+            <Form.Control
+              type="email"
+              value={input}
+              onChange={event => setInput(event.target.value)}
+              placeholder="Enter your email"
+              className="email__input"
+            />
+            {email && <span style={{ color: 'white' }}>{email}</span>}
+          </Form.Group>
+          <Button variant="light" type="submit" className="email__button">
+            Subscribe now
+          </Button>
+        </Form>
+      </Container>
+    </div>
+  );
+};
